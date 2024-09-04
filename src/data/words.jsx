c7b8144d725c1623d267208181013c2acaa04f12
Luis Jonson <luislindonjonson@yahoo.br>
2024-09-03 21:37:30 -0300

    Criando o projeto

---
+++ src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    computador: ["Mouse", "Teclado", "monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "javaScript", "React"],
+    alimento: ["Arroz", "feijão", "Carne", "Leite", "Ovo"],
+}
