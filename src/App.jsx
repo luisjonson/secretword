fd01bc621db0ce9ce9ed138528b9a9a4d6d3e4c2
Luis Jonson <luislindonjonson@yahoo.br>
2024-09-08 10:34:00 -0300

    Evento de inserção de letra

--- src/App.jsx
+++ src/App.jsx
@@ -69,8 +69,8 @@ function App() {
   }
 
   //process the letter input
-  const verifyLetter = () => {
-    setGameStage(stages[2].name)
+  const verifyLetter = (letter) => {
+    console.log(letter)
   }
 
   //restarts the game 
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,3 +1,4 @@
+import { useState, useRef } from "react"
 import "./Game.css"
 
 export const Game = ({ 
@@ -10,6 +11,15 @@ export const Game = ({
   guesses,
   score 
   }) => {
+    const [letter,setLetter] = useState("")
+    const letterInputRef = useRef(null)
+
+    const handleSubmit = (e) =>{
+      e.preventDefault();
+      verifyLetter(letter)
+      setLetter("")
+      letterInputRef.current.focus();
+    }
   return (
     <div className="game">
       <p className="ponts">
@@ -34,8 +44,16 @@ export const Game = ({
       </div>
       <div className="letterContainer">
         <p>Tente adivinha uma letra da palavra:</p>
-        <form>
-          <input type="text" name="letter" maxLength="1" required/>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text"
+            name="letter"
+            maxLength="1" 
+            required 
+            onChange={(e) => setLetter(e.target.value)}
+            value={letter}
+            ref={letterInputRef}
+          />
           <button>Jogar!</button>
         </form>
       </div>
